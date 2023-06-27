80200103ef7dd77c78e5321ee7db721d8c08c9cd
Nickolas Abad <123907645+NickolasAbad@users.noreply.github.com>
2023-06-26 21:47:59 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header/Header'
+import Formulario from './components/Formulario/Formulario'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Formulario />
+    </>
+  )
+
+}
+
+export default App
